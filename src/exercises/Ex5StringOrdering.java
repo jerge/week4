9fccaa07551cb042121f390e3a93eae52e837b91
Jerge <erikjer@student.chalmers.se>
2017-09-19 11:01:10 +0200

    ex5

--- src/exercises/Ex5StringOrdering.java
+++ src/exercises/Ex5StringOrdering.java
@@ -15,7 +15,7 @@ public class Ex5StringOrdering {
     }
 
     void program() {
-        /*
+
         // Yes, "aa bb cc" is ordered like "abc" because all
         // a's are before all b's that are before all c's
         out.println(isOrdered("abc", "aa bb cc"));
@@ -35,8 +35,22 @@ public class Ex5StringOrdering {
         out.println(isOrdered("a", "aa bb cc"));
         out.println(isOrdered("", "aa bb cc"));
         out.println(isOrdered("abc", ""));
-        out.println(!isOrdered("ax", "aa bb cc"));
-        */
+        out.println(!isOrdered("ax", "aa bb cc"));       // Wrong in assignment, second last statement returns true on the same basis
+
+    }
+
+    private boolean isOrdered(String key, String text) {
+        text = text.trim();
+        String[] keyArr = key.split("");
+        String[] textArr = text.split("");
+        for (int j = 0; j < keyArr.length-1; j++) {
+            for (int i = 0; i < textArr.length; i++) {
+                if (textArr[i].equals(keyArr[j]) && text.substring(0, i).contains(keyArr[j+1])) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
